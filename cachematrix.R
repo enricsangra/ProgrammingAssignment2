0768e0d3224179b9e190ca67c3c096bf58bf06b4
enricsangra <enricsangra@gmail.com>
2015-08-20 20:54:20 +0100

    Implemented makeCacheMatrix and cacheSolve methods.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As no structure was specified for the special matrix I decided only to add
+## the inverse method to it. The content of the matrix can't be modified as was
+## not required for the task.
+##
+## The cache is done in the same special matrix without need of the cacheSolve.
+## The cacheSolve will only invoke the method.
 
-## Write a short comment describing this function
-
+## Returns a list with a method inverse which will always return the inverse
+## of the matrix currently passed.
+## The inverse is not calculated until the method is executed.
+## Once calculated, the same result will be always returned.
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInverse <- NULL
+  
+  inverse <- function () {
+    if (is.null(cachedInverse)) {
+      cachedInverse <<- solve(x)
+    }
+    cachedInverse
+  }
+  
+  list(inverse = inverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns the result of the inverse method of the special matrix passed.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  x$inverse()
 }
